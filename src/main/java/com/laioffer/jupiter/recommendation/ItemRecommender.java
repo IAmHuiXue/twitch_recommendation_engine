d9071ee036b468ed0837ef5e4dff25294e747331
yuhuiyingri <xuehm05@gmail.com>
2021-07-31 14:34:18 -0400

    initialize

---
+++ src/main/java/com/laioffer/jupiter/recommendation/ItemRecommender.java
@@ -0,0 +1,160 @@
+package com.laioffer.jupiter.recommendation;
+
+import com.laioffer.jupiter.db.MySQLConnection;
+import com.laioffer.jupiter.db.MySQLException;
+import com.laioffer.jupiter.entity.Game;
+import com.laioffer.jupiter.entity.Item;
+import com.laioffer.jupiter.entity.ItemType;
+import com.laioffer.jupiter.external.TwitchClient;
+import com.laioffer.jupiter.external.TwitchException;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ItemRecommender {
+    // a few constants as default limit for recommendation
+    private static final int DEFAULT_GAME_LIMIT = 3;
+    private static final int DEFAULT_PER_GAME_RECOMMENDATION_LIMIT = 10;
+    private static final int DEFAULT_TOTAL_RECOMMENDATION_LIMIT = 20;
+
+    // Return a list of Item objects for the given type. Types are one of [Stream, Video, Clip].
+    // Add items are related to the top games provided in the argument
+    private List<Item> recommendByTopGames(ItemType type, List<Game> topGames) throws RecommendationException {
+        List<Item> recommendedItems = new ArrayList<>();
+        TwitchClient client = new TwitchClient();
+
+        outerloop:
+        for (Game game : topGames) {
+            List<Item> items;
+            try {
+                items = client.searchByType(game.getId(), type, DEFAULT_PER_GAME_RECOMMENDATION_LIMIT);
+            } catch (TwitchException e) {
+                throw new RecommendationException("Failed to get recommendation result");
+            }
+            for (Item item : items) {
+                if (recommendedItems.size() == DEFAULT_TOTAL_RECOMMENDATION_LIMIT) {
+                    break outerloop; // this can break the outerloop
+                }
+                recommendedItems.add(item);
+            }
+        }
+        return recommendedItems;
+    }
+
+    // Return a list of Item objects for the given type. Types are one of [Stream, Video, Clip].
+    // All items are related to the items previously favorite by the user.
+    // E.g., if a user favorite some videos about game "Just Chatting",
+    // then it will return some other videos about the same game.
+    private List<Item> recommendByFavoriteHistory(
+            Set<String> favoritedItemIds, List<String> favoriteGameIds, ItemType type) throws RecommendationException {
+        // Count the favorite game IDs from the database for the given user.
+        // E.g. if the favorite game ID list is ["1234", "2345", "2345", "3456"],
+        // the returned Map is {"1234": 1, "2345": 2, "3456": 1}
+
+        // same as for loop + getOrDefault
+        // new way starting from Java 8
+        // firstly group by the key (game id), then collect the value (count)
+        Map<String, Long> favoriteGameIdByCount = favoriteGameIds.parallelStream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+
+        // Sort the game id by count in descending order.
+        // E.g. if the input is {"1234": 1, "2345": 2, "3456": 1}, the returned Map is {"2345": 2, "1234": 1, "3456": 1}
+        // store entrySet to a List to invoke sort function provided in List
+        List<Map.Entry<String, Long>> sortedFavoriteGameIdListByCount = new ArrayList<>(
+                favoriteGameIdByCount.entrySet());
+        sortedFavoriteGameIdListByCount.sort((Map.Entry<String, Long> e1, Map.Entry<String, Long> e2) -> Long
+                .compare(e2.getValue(), e1.getValue()));
+        // See also: https://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values
+
+        if (sortedFavoriteGameIdListByCount.size() > DEFAULT_GAME_LIMIT) {
+            sortedFavoriteGameIdListByCount = sortedFavoriteGameIdListByCount.subList(0, DEFAULT_GAME_LIMIT);
+        }
+
+        List<Item> recommendedItems = new ArrayList<>();
+        TwitchClient client = new TwitchClient();
+
+        // Search Twitch based on the favorite game IDs returned to the last step.
+        outerloop:
+        for (Map.Entry<String, Long> favoriteGame : sortedFavoriteGameIdListByCount) {
+            List<Item> items;
+            try {
+                items = client.searchByType(favoriteGame.getKey(), type, DEFAULT_PER_GAME_RECOMMENDATION_LIMIT);
+            } catch (TwitchException e) {
+                throw new RecommendationException("Failed to get recommendation result");
+            }
+
+            for (Item item : items) {
+                if (recommendedItems.size() == DEFAULT_TOTAL_RECOMMENDATION_LIMIT) {
+                    break outerloop;
+                }
+                if (!favoritedItemIds.contains(item.getId())) {
+                    recommendedItems.add(item);
+                }
+            }
+        }
+        return recommendedItems;
+    }
+
+    // If the user is successfully logged in, recommend by the favorite records,
+    // Return a map of Item objects as the recommendation result. Keys of the map are [Stream, Video, Clip].
+    // Each key is corresponding to a list of Items objects,
+    // each item object is a recommended item based on the previous favorite records by the user.
+    public Map<String, List<Item>> recommendItemsByUser(String userId) throws RecommendationException {
+        Map<String, List<Item>> recommendedItemMap = new HashMap<>();
+        Set<String> favoriteItemIds;
+        Map<String, List<String>> favoriteGameIds;
+        MySQLConnection connection = null;
+        try {
+            connection = new MySQLConnection();
+            favoriteItemIds = connection.getFavoriteItemIds(userId);
+            favoriteGameIds = connection.getFavoriteGameIds(favoriteItemIds);
+        } catch (MySQLException e) {
+            throw new RecommendationException("Failed to get user favorite history for recommendation");
+        } finally {
+            connection.close();
+        }
+
+        for (Map.Entry<String, List<String>> entry : favoriteGameIds.entrySet()) {
+            // does not have favorite records
+            // recommend by default twitch top games
+            if (entry.getValue().size() == 0) {
+                TwitchClient client = new TwitchClient();
+                List<Game> topGames;
+                try {
+                    topGames = client.topGames(DEFAULT_GAME_LIMIT);
+                } catch (TwitchException e) {
+                    throw new RecommendationException("Failed to get game data for recommendation");
+                }
+                recommendedItemMap.put(entry.getKey(),
+                        recommendByTopGames(ItemType.valueOf(entry.getKey()), topGames));
+            } else {
+                recommendedItemMap.put(entry.getKey(),
+                        recommendByFavoriteHistory(favoriteItemIds, entry.getValue(),
+                                ItemType.valueOf(entry.getKey())));
+            }
+        }
+        return recommendedItemMap;
+    }
+    // If the user is not logged in, recommend by the top games.
+    // Return a map of Item objects as the recommendation result. Keys of the map are [Stream, Video, Clip].
+    // Each key is corresponding to a list of Items objects,
+    // each item object is a recommended item based on the top games currently on Twitch.
+    public Map<String, List<Item>> recommendItemsByDefault() throws RecommendationException {
+        Map<String, List<Item>> recommendedItemMap = new HashMap<>();
+        TwitchClient client = new TwitchClient();
+        List<Game> topGames;
+        try {
+            topGames = client.topGames(DEFAULT_GAME_LIMIT);
+        } catch (TwitchException e) {
+            throw new RecommendationException("Failed to get game data for recommendation");
+        }
+
+        for (ItemType type : ItemType.values()) {
+            recommendedItemMap.put(type.toString(), recommendByTopGames(type, topGames));
+        }
+        return recommendedItemMap;
+    }
+
+}
